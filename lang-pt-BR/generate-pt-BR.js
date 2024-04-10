994e108d85a7ab02b5cb78c26d1995342ed5abc1
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-04-10 17:13:33 +0200

    Portuguese

---
+++ lang-pt-BR/generate-pt-BR.js
@@ -0,0 +1,11 @@
+import { loadBlacklist } from "../blacklist/blacklist.js";
+import { processCorpus } from "../lib/corpus.js";
+import { ptBR } from "./pt-BR.js";
+
+await processCorpus({
+  language: ptBR,
+  file: ["/home/caustic/Downloads/corpus/opensubtitles/pt.txt"],
+  blacklist: loadBlacklist()
+    .addFile("lang-pt-BR/blacklist-profanity.txt")
+    .addFile("lang-pt-BR/blacklist-english.txt"),
+});
--- lang-pt-BR/pt-BR.js
+++ lang-pt-BR/pt-BR.js
@@ -3,7 +3,7 @@ import { Language } from "../lib/language.js";
 const reAlphabet = /^[aáâãàbcçdeéêfghiíjklmnoóôõpqrstuúvwxyz]+$/u;
 const reVowels = /[AÁÂÃÀEÉÊIÍJOÓÔÕUÚaáâãàeéêiíjoóôõuú]/;
 
-export function testWord_en(word) {
+export function testWord_pt_BR(word) {
   if (word.length > 20) {
     return false;
   }
@@ -16,4 +16,4 @@ export function testWord_en(word) {
   return true;
 }
 
-export const pt = new Language("pt", testWord_en);
+export const ptBR = new Language("pt-BR", testWord_pt_BR);
