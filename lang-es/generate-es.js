0c84b15915e4f281b4554afceae96df60471717c
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-04-16 18:27:43 +0200

    Spanish language

---
+++ lang-es/generate-es.js
@@ -0,0 +1,11 @@
+import { loadBlacklist } from "../blacklist/blacklist.js";
+import { processCorpus } from "../lib/corpus.js";
+import { es } from "./es.js";
+
+await processCorpus({
+  language: es,
+  file: ["~/Downloads/corpus/opensubtitles/es.txt"],
+  blacklist: loadBlacklist()
+    .addFile("lang-es/blacklist.txt")
+    .addFile("blacklist/english.txt"),
+});
