b440433fd10b54fd8de4077cfe6d672af120abb7
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-04-30 14:21:43 +0200

    English rare words

--- lang-en/mix.js
+++ lang-en/mix.js
@@ -7,14 +7,20 @@ import { sortByCount } from "../lib/dict.js";
 import { pathTo, readDict, readLines, writeDict } from "../lib/io.js";
 
 const dict = new Map(await readDict(pathTo("lang-en/dictionary-en-books.csv")));
-const wj = ["j", await readWords(pathTo("lang-en/words-j.txt"))];
-const wq = ["q", await readWords(pathTo("lang-en/words-q.txt"))];
-const wx = ["x", await readWords(pathTo("lang-en/words-x.txt"))];
-const wz = ["z", await readWords(pathTo("lang-en/words-z.txt"))];
+
+const mix = [
+  ["f", await readWords(pathTo("lang-en/words-f.txt"))],
+  ["j", await readWords(pathTo("lang-en/words-j.txt"))],
+  ["q", await readWords(pathTo("lang-en/words-q.txt"))],
+  ["v", await readWords(pathTo("lang-en/words-v.txt"))],
+  ["w", await readWords(pathTo("lang-en/words-w.txt"))],
+  ["x", await readWords(pathTo("lang-en/words-x.txt"))],
+  ["z", await readWords(pathTo("lang-en/words-z.txt"))],
+];
 
 const keep = new Set();
 
-for (const [letter, list] of [wj, wq, wx, wz]) {
+for (const [letter, list] of mix) {
   console.log(`====== ${letter} ======`);
   process(letter, list);
 }
@@ -26,13 +32,13 @@ await writeDict(pathTo("lang-en/dictionary-en.csv"), sortByCount([...dict]));
 function process(letter, list) {
   for (const word of dict.keys()) {
     if (word.includes(letter)) {
-      if (!list.has(word)) {
-        dict.delete(word);
-        console.log(`Deleted word ${chalk.red(word)}`);
-      }
+      keep.add(word);
     }
   }
   for (const word of list) {
+    if (!word.includes(letter)) {
+      throw new Error(`Word ${word} does not include letter ${letter}`);
+    }
     if (!dict.has(word)) {
       dict.set(word, 1);
       console.log(`Added word ${chalk.green(word)}`);
@@ -47,6 +53,7 @@ function trimDict() {
     for (const word of words) {
       if (dict.has(word) && !keep.has(word)) {
         dict.delete(word);
+        console.log(`Deleted word ${chalk.red(word)}`);
         break;
       }
     }
