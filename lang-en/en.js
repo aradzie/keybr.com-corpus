48e7e560caa4448c685180196234f16f0c549506
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-11-20 20:07:57 +0100

    English language

--- lang-en/en.js
+++ lang-en/en.js
@@ -1,16 +1,10 @@
 import { Language } from "../lib/language.js";
-import { frequent } from "./frequent.js";
-
-const frequentSet = new Set(frequent.slice(0, 5000));
 
 const reAlphabet = /^[abcdefghijklmnopqrstuvwxyz]+$/u;
 const reVowels = /[AEIOUYaeiouy]/;
 
 export const en = new Language("en", (word) => {
-  if (frequentSet.has(word) || frequentSet.has(word.toLocaleLowerCase("en"))) {
-    return true;
-  }
-  if (word.length > 20) {
+  if (word.length > 15) {
     return false;
   }
   if (!reAlphabet.test(word)) {
--- lang-en/generate-en.js
+++ lang-en/generate-en.js
@@ -5,10 +5,10 @@ import { en } from "./en.js";
 await processCorpus({
   language: en,
   files: [
-    "~/Downloads/corpus/books_large_p1.txt",
-    "~/Downloads/corpus/books_large_p2.txt",
-    // "~/Downloads/corpus/enwiki-20220201-clean.txt",
-    // "~/Downloads/corpus/1-billion-word-language-modeling-benchmark-r13output/**/*",
+    "/mnt/userdata/corpus/books_large_p1.txt",
+    "/mnt/userdata/corpus/books_large_p2.txt",
+    // "/mnt/userdata/corpus/enwiki-20220201-clean.txt",
+    // "/mnt/userdata/corpus/1-billion-word-language-modeling-benchmark-r13output/**/*",
   ],
   blacklist: loadBlacklist().addFiles("lang-en/blacklist.txt"),
 });
