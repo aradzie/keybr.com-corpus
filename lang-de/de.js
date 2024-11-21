e2524ba3e57d80c06f83f3c2ecf273666494e311
Aliaksandr Radzivanovich <aradzivanovich@gmail.com>
2024-11-21 16:42:41 +0100

    German langauge

--- lang-de/de.js
+++ lang-de/de.js
@@ -1,17 +1,8 @@
 import { Language } from "../lib/language.js";
 
-const reAlphabet = /^[A-ZÄÖÜ]?[a-zäöüß]+$/;
-const reVowels = /[AEIOUÄÖÜaeiouäöü]/;
-
 export const de = new Language("de", (word) => {
   if (word.length < 2 || word.length > 15) {
     return false;
   }
-  if (!reAlphabet.test(word)) {
-    return false;
-  }
-  if (!reVowels.test(word)) {
-    return false;
-  }
-  return true;
+  return /^[A-ZÄÖÜ]?[a-zäöüß]+$/.test(word) && /[aeiouäöü]/.test(word);
 });
